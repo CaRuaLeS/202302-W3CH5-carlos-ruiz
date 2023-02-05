0ae3cce666bca9162f290cb3207bd8c28e8223fb
CarlosRuiz <carua97@gmail.com>
2023-02-05 12:30:31 +0100

    Add item pokemon

--- src/components/03.item/element.ts
+++ src/components/03.item/element.ts
@@ -1,1 +1,28 @@
+import { PokemonStructure } from '../../models/pokemon';
+import { Component } from '../00.component/component';
 
+export class PokemonItem extends Component {
+  constructor(
+    public selector: string,
+    public pokemonList: PokemonStructure,
+    public updateTask: (task: TaskStructure) => void
+  ) {
+    super();
+    this.pokemonList = { ...pokemonList };
+    this.template = this.createTemplate();
+    this.render('afterbegin');
+  }
+
+  private createTemplate() {
+    return `
+      <li class="pokemon-item">
+        <img src="" alt="Pokemon">
+        <div>Pokemon name</div>
+      </li>
+    `;
+  }
+
+  render(place: globalThis.InsertPosition) {
+    super.render(place);
+  }
+}
